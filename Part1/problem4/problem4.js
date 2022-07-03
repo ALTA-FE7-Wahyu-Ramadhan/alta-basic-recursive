6f46393b278f1fdfe152d5b3e7cc195a69780a68
unknown <wahyuramadhan0549@gmail.com>
2022-07-04 00:53:02 +0700

    problem4.js was cleared

--- Part1/problem4/problem4.js
+++ Part1/problem4/problem4.js
@@ -1,5 +1,19 @@
 function maxSequence(arr) {
   // Your code here
-}
+  //deklarasiin dulu yang mau di loop
+  let currentSum = 0;
+  let largestSum = 0;
+  
+  for (let number of arr) {
+    currentSum = Math.max(0, currentSum + number);
+    largestSum = Math.max(largestSum, currentSum);
+  }
 
-module.exports = maxSequence;
+  return largestSum;
+}
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); //6
+console.log(maxSequence([-2, -5, 6, -2, -3, 1, 5, -6])); //7
+console.log(maxSequence([-2, -3, 4, -1, -2, 1, 5, -3])); //7
+console.log(maxSequence([-2, -5, 6, -2, -3, 1, 6, -6])); //8
+console.log(maxSequence([-2, -5, 6, 2, -3, 1, 6, -6])); //12
+module.exports = maxSequence
