0293e6a8a88cce3b5579edccafa79b0df5a65e09
unknown <wahyuramadhan0549@gmail.com>
2022-07-04 00:57:09 +0700

    problem4 was cleared

--- Part1/problem4/problem4.js
+++ Part1/problem4/problem4.js
@@ -8,7 +8,6 @@ function maxSequence(arr) {
     currentSum = Math.max(0, currentSum + number);
     largestSum = Math.max(largestSum, currentSum);
   }
-
   return largestSum;
 }
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); //6
