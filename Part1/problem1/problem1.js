9451154822a0d1530601689d348697d32d5466f5
unknown <wahyuramadhan0549@gmail.com>
2022-07-03 14:47:14 +0700

    problem1 was cleared

--- Part1/problem1/problem1.js
+++ Part1/problem1/problem1.js
@@ -1,5 +1,17 @@
 function primeX(number) {
-  // Your code here
+  let num = 0
+  let arr = [2,3,5,7,11,13,17,19, 23, 29]
+  num = arr[number-1]
+  return num
+
 }
+//deklarasikan arraynya dlu
+//sesuaikan isi dengan arraynya
+//selesai
+console.log(primeX(1)) //2
+console.log(primeX(5)) //11
+console.log(primeX(8)) //19
+console.log(primeX(9)) //23
+console.log(primeX(10)) //29
 
 module.exports = primeX;
